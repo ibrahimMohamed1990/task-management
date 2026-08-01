3dcef5a5c93f58f17d0ddb22ffb53490e4a5d444
ibrahimMohamed1990 <himamohamed1991@gmail.com>
2026-08-01 16:02:33 +0300

    add resources

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'project_id' => $this->project_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'priority' => $this->priority,
+            'status' => $this->status,
+            'due_date' => $this->due_date?->toDateString(),
+            'is_overdue' => $this->is_overdue,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
